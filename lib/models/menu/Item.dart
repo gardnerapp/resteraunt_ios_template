d9ffde6acc4bb14796141b07ac5eb8147cbcb986
Corey Gardner <coreygardner@Coreys-MacBook-Pro.local>
2021-05-05 12:13:55 -0400

    add image field to item model

--- lib/models/menu/Item.dart
+++ lib/models/menu/Item.dart
@@ -6,29 +6,27 @@ class Item {
   final int id;
   final String name;
   final double price;
-  final String description;
-  /*final List<SideCollection> sideCollections;
-  final List<Extra> extras;
-*/
-  Item(
-      {this.id,
-      this.name,
-      this.price,
-      this.description,
-      //this.sideCollections,
-     // this.extras
-      });
+  String coverPhoto;
+  String description;
+
+  Item({
+    this.id,
+    this.name,
+    this.price,
+    this.coverPhoto,
+    this.description,
+  });
 
   factory Item.fromJson(Map<String, dynamic> json) {
     return Item(
         id: json['id'],
         name: json['name'],
         price: json['price'],
-      /*  description: json['description'],
-        sideCollections: json['sideCollections']
-            .map((e) => SideCollection.fromJson(e))
-            .toList(),
-        extras: json['extras'].map((e) => Extra.fromJson(e)).toList()*/
+        coverPhoto: json['image'],
+        description: json['description']
     );
   }
 }
+
+
+
